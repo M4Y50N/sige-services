fd2b2644700e0e7555d5cacf9b1050e13f3730cb
M4Y50N <pethersonreis@gmail.com>
2023-01-06 11:25:35 -0300

    refactor: json server

--- user_services/api/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from user_services.api import serializers
-from user_services import models
-
-class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.UserSerializer
-    queryset = models.User.objects.all()
--- user_services/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from uuid import uuid4
-
-
-class User(models.Model):
-    user_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    user_name = models.CharField(max_length=255)
-    user_email = models.EmailField(max_length=255)
-    user_password = models.CharField(max_length=50)
