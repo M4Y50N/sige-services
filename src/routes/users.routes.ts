0d867b7a5e64c99ba72b0259d5b15ee87ad02c4f
pethe <pethersonreis@gmail.com>
2023-04-01 17:03:47 -0300

    feat: get one user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,8 @@ import deleteUserController from "../controllers/users/deleteUser.controller";
 import getAllUsersController from "../controllers/users/getAllUsers.controller";
 import ensureAdminMiddleware from "../middlewares/ensureAdmin.middleware";
 import ensureTokenIsValidMiddleware from "../middlewares/ensureTokenIsValid.middleware";
+import getUserController from "../controllers/users/getUser.controller";
+import ensureUserExistsMiddleware from "../middlewares/ensureUserExists.middleware";
 
 const usersRoutes: Router = Router();
 
@@ -13,6 +15,7 @@ usersRoutes.get(
 	ensureAdminMiddleware,
 	getAllUsersController
 );
+usersRoutes.get("/:id", ensureUserExistsMiddleware, getUserController);
 
 usersRoutes.post("", createUserController);
 
