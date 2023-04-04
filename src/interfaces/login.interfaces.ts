3c1816f0370c42235c0c41e72f2ae4971b3fe579
pethe <pethersonreis@gmail.com>
2023-04-04 00:37:57 -0300

    api_doc

--- src/interfaces/login.interfaces.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-import { loginSchema } from "../schemas/login.schema";
-
-type tLogin = z.infer<typeof loginSchema>;
-
-export { tLogin };
--- src/routes/login.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import loginController from "../controllers/login.controller";
-
-const loginRoutes: Router = Router();
-
-loginRoutes.post("", loginController);
-
-export default loginRoutes;
