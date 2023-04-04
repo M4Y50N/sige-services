b1bc6e6395ad0c4c6f3a20a73c5ca0acd5e99821
pethe <pethersonreis@gmail.com>
2023-04-03 22:42:13 -0300

    feat: get event route

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import createEventsController from "../controllers/events/createEvent.controller";
 import deleteEventController from "../controllers/events/deleteEvent.controller";
 import getAllEventsController from "../controllers/events/getAllEvents.controller";
+import getEventController from "../controllers/events/getEvent.controller";
 
 const eventsRoutes: Router = Router();
+
+eventsRoutes.get("/:id", getEventController);
 
 eventsRoutes.get("", getAllEventsController);
 
 eventsRoutes.post("", createEventsController);
 
-eventsRoutes.delete("", deleteEventController);
+eventsRoutes.delete("/:id", deleteEventController);
 
 export default eventsRoutes;
